664c37121eb333ad96f2be6b5434b2b088a7b47b
pinklad <stevenswawerus@gmail.com>
2023-05-26 14:33:07 +0300

    Changes to code

--- builtin.c
+++ builtin.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * _myexit - exits
- * @info: Structure
- * constant function prototype.
- * Return: exits with a status
- * (0) if info.argv[0] != "exit"
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1]) /* If there is an exit arguement */
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -30,10 +30,10 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes process
- * @info: Used to maintain
- * constant function prototype.
- * Return: Always 0
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _mycd(info_t *info)
 {
@@ -81,9 +81,9 @@ int _mycd(info_t *info)
 
 /**
  * _myhelp - changes the current directory of the process
- * @info: Structure
- * function prototype.
- * Return: Always 0
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _myhelp(info_t *info)
 {
--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 /**
  * main - entry point
- * @ac: count
- * @av: vector
+ * @ac: arg count
+ * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
@@ -13,9 +13,9 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
